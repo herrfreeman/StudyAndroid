144330c67e210fe5036efd125083791127d66704
herrfreeman <freeman@blackmesa.ru>
2024-01-23 00:18:22 +0300

    rebuild main activity to recyclerview

--- app/src/main/java/com/practicum/studyandroid/MainActivity.kt
+++ app/src/main/java/com/practicum/studyandroid/MainActivity.kt
@@ -3,49 +3,66 @@ package com.practicum.studyandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
 import com.practicum.studyandroid.imdb.MovieSearch
+import com.practicum.studyandroid.weather.WeatherActivity
 import com.practicum.studyandroid.weather.WeatherLocation
 
+fun interface ClickListener {
+    fun onClick()
+}
+
+data class ActionItem(val name: String, val clickListener: ClickListener)
+
 class MainActivity : AppCompatActivity() {
+
+    val actionList = mutableListOf<ActionItem>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startPrimeNumbers)
-        .setOnClickListener {
-            startActivity(Intent(this, PrimeNumber::class.java))
-        }
+        val recycler = findViewById<RecyclerView>(R.id.mainRecyclerView)
+        recycler.adapter = MainViewHolderAdapter(actionList)
 
-        findViewById<Button>(R.id.startListExample)
-            .setOnClickListener {
-                startActivity(Intent(this, ListExamples::class.java))
-            }
+        actionList.add(ActionItem("Prime numbers") {startActivity(Intent(this, PrimeNumber::class.java))})
+        actionList.add(ActionItem("List example") {startActivity(Intent(this, ListExamples::class.java))})
+        actionList.add(ActionItem("ViewHolder example") {startActivity(Intent(this, ViewHolderExample::class.java))})
 
-        findViewById<Button>(R.id.recycleViewExample)
-            .setOnClickListener {
-                startActivity(Intent(this, ViewHolderExample::class.java))
-            }
+        actionList.add(ActionItem("ViewHolder example") {startActivity(Intent(this, ViewHolderExample::class.java))})
+        actionList.add(ActionItem("Weather") {startActivity(Intent(this, WeatherActivity::class.java))})
+        actionList.add(ActionItem("Weather location API") {startActivity(Intent(this, WeatherLocation::class.java))})
+        actionList.add(ActionItem("IMDB API") {startActivity(Intent(this, MovieSearch::class.java))})
+        actionList.add(ActionItem("RelativeLayout example") {startActivity(Intent(this, RelativeExample::class.java))})
+        actionList.add(ActionItem("ConstraintLayout example") {startActivity(Intent(this, ConstraintLayoutExample::class.java))})
 
-        findViewById<Button>(R.id.weather)
-            .setOnClickListener {
-                //startActivity(Intent(this, WeatherActivity::class.java))
-                startActivity(Intent(this, WeatherLocation::class.java))
-            }
+    }
+}
 
-        findViewById<Button>(R.id.movies)
-            .setOnClickListener {
-                startActivity(Intent(this, MovieSearch::class.java))
-            }
+class MainViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(parentView.context)
+    .inflate(R.layout.main_recycler_element, parentView, false)) {
 
-        findViewById<Button>(R.id.rellayout)
-            .setOnClickListener {
-                startActivity(Intent(this, RelativeExample::class.java))
-            }
+    var button: Button
 
-        findViewById<Button>(R.id.conslayout)
-            .setOnClickListener {
-                startActivity(Intent(this, ConstraintLayoutExample::class.java))
-            }
+    init {
+        button = itemView.findViewById(R.id.runButton)
+    }
+
+    fun bind(model: ActionItem) {
+        button.text = model.name
+        button.setOnClickListener { model.clickListener.onClick() }
+    }
+}
+
+
+class MainViewHolderAdapter(val actionList: List<ActionItem>) : RecyclerView.Adapter<MainViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder = MainViewHolder(parent)
+    override fun getItemCount(): Int = actionList.size
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.bind(actionList[position])
     }
 }
